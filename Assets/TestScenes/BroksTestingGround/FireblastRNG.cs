a3d297b7a1d850c9275dfb4fa3880fbfa1d23478
Brok Field <20.brok.field@ksd.org>
2020-01-09 12:10:18 -0800

    adding scripts

---
+++ Assets/TestScenes/BroksTestingGround/FireblastRNG.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireblastRNG : MonoBehaviour
+{
+    public Transform player;
+    public GameObject prefab;
+    public float bulletSpeed = 10.0f;
+    public float bulletLifetime = 1.0f;
+    public float shootDelay = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player.Position.x;
+        //player.Position.y;
+        Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject bullet = Instantiate(prefab, transform.position, Quaternion.identity);
+        Vector2 shootDir = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
+        shootDir.Normalize();
+        bullet.GetComponent<Rigidbody2D>().velocity = shootDir * bulletSpeed;
+        Destroy(bullet, bulletLifetime);
+    } 
+}
---
+++ Assets/TestScenes/BroksTestingGround/Threeshotblast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Threeshotblast : MonoBehaviour
+{
+    public GameObject prefab;
+    public float numShots = 5;            // Number of shots fired (should be odd); 
+    public float spreadAngle = 2.0f;       // Angle between shots
+    public float timeBetweenShots = 0.5f;  // Minimum time between shots
+    public float nextShot = 0.0f;
+    void Start()
+    {
+        if (numShots / 2 * 2 == numShots) numShots++; // Need an odd number of shots
+        if (numShots < 3) numShots = 3;  // At least 3 shots for a fan
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextShot)
+        {
+
+            nextShot = Time.time + timeBetweenShots;
+            var qAngle = Quaternion.AngleAxis(-numShots / 2.0f * spreadAngle, transform.up) * transform.rotation;
+            var qDelta = Quaternion.AngleAxis(spreadAngle, transform.up);
+            for (var i = 0; i < numShots; i++)
+            {
+                var go = (GameObject)Instantiate(prefab, transform.position, qAngle);
+                go.rigidbody.AddForce(go.transform.forward * 1000.0);
+                qAngle = qDelta * qAngle;
+            }
+        }
+    }
+}
