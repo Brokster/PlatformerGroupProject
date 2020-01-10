7f574f4ad44b5f09668bd699b073b62974b957bc
Dalton Haer <20.dalton.haer@ksd.org>
2020-01-10 12:48:15 -0800

    added the following sprites for the player: climb, damage, death, falling, idle, jump, run, shoot and win.

---
+++ Assets/C#Scripts/goalScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goalScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "b1goal")
+        {
+            SceneManager.LoadScene("board-2");
+        }
+        if (collision.gameObject.tag == "b2goal")
+        {
+            SceneManager.LoadScene("board-3");
+        }
+        if (collision.gameObject.tag == "b3goal")
+        {
+            SceneManager.LoadScene("win");
+        }
+    }
+   
+}
--- Assets/C#Scripts/levelSwitchingScript.cs
+++ Assets/C#Scripts/levelSwitchingScript.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class levelSwitchingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool fire1complete = false;
+    public bool fire2complete = false;
+    public bool water1complete = false;
+    public bool water2complete = false;
+    public bool sand1complete = false;
+    public bool sand2complete = false;
 
-    // Update is called once per frame
-    void Update()
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "fire1")
+        {
+            SceneManager.LoadScene("stageSelect");
+            bool fire1complete = true;
+        }
+        if (collision.gameObject.tag == "fire2")
+        {
+            SceneManager.LoadScene("stageSelect");
+            bool fire2complete = true;
+        }
+        if (collision.gameObject.tag == "water1")
+        {
+            SceneManager.LoadScene("stageSelect");
+            bool water1complete = true;
+        }
+        if (collision.gameObject.tag == "water2")
+        {
+            SceneManager.LoadScene("stageSelect");
+            bool water2complete = true;
+        }
+        if (collision.gameObject.tag == "sand1")
+        {
+            SceneManager.LoadScene("stageSelect");
+            bool sand1complete = true;
+        }
+        if (collision.gameObject.tag == "sand2")
+        {
+            SceneManager.LoadScene("stageSelect");
+            bool sand2complete = true;
+        }
     }
+    private void
+    {
+       if bool fire1complete = true && bool fire2complete = true && bool water1complete = true && bool water2complete = true && bool sand1complete = true && sand2complete = true;
 }
+
+
