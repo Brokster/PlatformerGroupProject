53de518507d43b27c1932dca6130176434f64383
Brok Field <20.brok.field@ksd.org>
2020-01-15 13:45:55 -0800

    adding in firemap progress scripts and prefabs

---
+++ Assets/TestScenes/BroksTestingGround/DisapperingPlatformScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisapperingPlatformScript : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            {
+                Destroy(gameObject, 2f);
+            }
+        }
+    }
+}
---
+++ Assets/TestScenes/BroksTestingGround/FallingPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatforms : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D (Collision col)
+    {
+        if (col.gameObject.name.Equals ("Player"))
+        {
+            Invoke("DropPlatform", 0.5f);
+            Destroy(gameObject, 2f);
+        }
+    }
+       void DropPlatform()
+    {
+        rb.isKinematic = false;
+    }
+}
