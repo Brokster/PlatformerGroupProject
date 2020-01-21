787851d7cac7342570a94285c8f689618ce7e13e
Dalton Haer <20.dalton.haer@ksd.org>
2020-01-21 11:42:26 -0800

    made the player a prefab and worked on level switching script.

--- Assets/C#Scripts/levelSwitchingScript.cs
+++ Assets/C#Scripts/levelSwitchingScript.cs
@@ -6,48 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class levelSwitchingScript : MonoBehaviour
 {
-    public bool fire1complete = false;
-    public bool fire2complete = false;
-    public bool water1complete = false;
-    public bool water2complete = false;
-    public bool sand1complete = false;
-    public bool sand2complete = false;
-
-
-
-
+    public bool stage1 = false;
+    public bool stage2 = false;
+    public bool stage3 = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "fire1")
         {
             SceneManager.LoadScene("stageSelect");
-            bool fire1complete = true;
-        }
-        if (collision.gameObject.tag == "fire2")
-        {
-            SceneManager.LoadScene("stageSelect");
-            bool fire2complete = true;
+            stage1 = true;
         }
         if (collision.gameObject.tag == "water1")
         {
             SceneManager.LoadScene("stageSelect");
-            bool water1complete = true;
-        }
-        if (collision.gameObject.tag == "water2")
-        {
-            SceneManager.LoadScene("stageSelect");
-            bool water2complete = true;
+            stage2 = true;
         }
         if (collision.gameObject.tag == "sand1")
         {
             SceneManager.LoadScene("stageSelect");
-            bool sand1complete = true;
+            stage3 = true;
         }
-        if (collision.gameObject.tag == "sand2")
-        {
-            SceneManager.LoadScene("stageSelect");
-            bool sand2complete = true;
-        }
+   
     }
 }
